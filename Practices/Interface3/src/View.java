d798df00d5d8ce29d9f5c218f6ce7d1753c24a71
Saayaman <ayakosayama1987@gmail.com>
2016-12-08 21:02:06 -0800

    Adding setters and getters

--- Practices/Interface3/src/View.java
+++ Practices/Interface3/src/View.java
@@ -10,48 +10,52 @@ import static java.awt.BorderLayout.*;
 /**
  * Created by ayako_sayama on 2016/12/07.
  */
-public class View extends Main{
-
-    private Listener listener;
-    private Object source;
+public class View{
 
     private JButton addPet;
     private DefaultTableModel pets;
+    private Object source;
+    //private addPetListner;
+
+    private JComboBox inputType;
+    private JTextField inputName;
+    private JTextField inputAge;
+    private JRadioButton soundYes;
+    private JRadioButton soundNo;
+
+    public int animalNumber = 0;
 
     public View() {
-        view2();
+        view();
     }
 
-    private void view2() {
+//    private void view2() {
+//
+//        String[] columnNames = {"Type", "Name", "Age", "Sound"};
+//
+//        JFrame petsFrame = new JFrame("Pets Database");
+//        //petsFrame.setTitle("aaa"); >> you can set Title here, but also inside the JFrame('');
+//
+//        petsFrame.setBounds( 10, 10, 300, 200);
+//        //setBounds(x.from mac screen, y.from mac screen, x.size, y.size)
+//
+//        DefaultTableModel tableModel
+//                = new DefaultTableModel(columnNames,0);
+//
+//        JTable table = new JTable(tableModel);
+//
+//        JScrollPane sp = new JScrollPane(table);
+//        sp.setPreferredSize(new Dimension(250, 70));
+//
+//        JPanel p = new JPanel();
+//        p.add(sp);
+//
+//        petsFrame.getContentPane().add(p, BorderLayout.CENTER);
+//
+//        petsFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        petsFrame.setVisible(true);
+//    }
 
-        String[][] tabledata = {
-                {"日本", "3勝", "0敗", "1分"},
-                {"クロアチア", "3勝", "1敗", "0分"},
-                {"ブラジル", "1勝", "2敗", "1分"},
-                {"オーストラリア", "2勝", "2敗", "0分"}};
-        String[] columnNames = {"COUNTRY", "WIN", "LOST", "EVEN"};
-
-        JFrame test = new JFrame("SwingTest");
-
-        test.setTitle("aaa");
-        test.setBounds( 10, 10, 300, 200);
-
-        DefaultTableModel tableModel
-                = new DefaultTableModel(tabledata, columnNames);
-
-        JTable table = new JTable(tableModel);
-
-        JScrollPane sp = new JScrollPane(table);
-        sp.setPreferredSize(new Dimension(250, 70));
-
-        JPanel p = new JPanel();
-        p.add(sp);
-
-        test.getContentPane().add(p, BorderLayout.CENTER);
-
-        test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        test.setVisible(true);
-    }
 
    private void view(){
 
@@ -62,66 +66,136 @@ public class View extends Main{
        JPanel panel = new JPanel(new BorderLayout());
        panel.setPreferredSize(new Dimension(500,500));
 
-       frame.getContentPane().add(panel);
-       frame.pack();
-       frame.setVisible(true);
-
 
        //adding panels
        JPanel tablePanel = new JPanel(new BorderLayout());
-       JPanel inputPanel = new JPanel(new CardLayout());
+       JPanel addPetPanel = new JPanel(new BorderLayout());
+       JPanel inputPanel = new JPanel(new GridLayout(2,2));
        JPanel buttonPanel = new JPanel(new BorderLayout());
 
        panel.add(tablePanel, BorderLayout.CENTER);
-       panel.add(inputPanel, BorderLayout.SOUTH);
-       //inputPanel.add(buttonPanel, BorderLayout.SOUTH);
+       panel.add(addPetPanel, BorderLayout.SOUTH);
+       addPetPanel.add(buttonPanel, BorderLayout.SOUTH);
+       addPetPanel.add(inputPanel, BorderLayout.NORTH);
        //2パネルと上と下に配置 / ボタンパネルを inputPanel の下に配置
 
 
        Object[] petColumns = {"Type", "Name", "Age", "Sound"};
+
        JTable table = new JTable(new DefaultTableModel(petColumns,0));
+       //DefaultTableModel(列、行)
+
+       pets = (DefaultTableModel)table.getModel();
        JScrollPane scrollPane = new JScrollPane(table);
        scrollPane.setPreferredSize(new Dimension(480,480));
-       pets = (DefaultTableModel)table.getModel();
+
        //scrollPane.add(table);
        //scrollable panel
 
 
        addPet = new JButton("Add to List");
-       JToggleButton inputType = new JToggleButton("Choose Animalt Type");
-       JTextField inputlName = new JTextField("Name");
-       JTextField inputAge = new JTextField("Age");
-       JRadioButton inputSound = new JRadioButton();
+
+
+       //スライドで選択
+       String[] animalType = {"Cat", "Dog" };
+       inputType = new JComboBox(animalType);
+       inputName = new JTextField("Name");
+       inputAge = new JTextField("Age");
+       JLabel inputSound = new JLabel("Makes Sound?:");
+
+       soundYes = new JRadioButton("Yes");
+       soundNo = new JRadioButton("No");
+
+       ButtonGroup bg1 = new ButtonGroup( ); //can only select one from buttongroup
+       bg1.add(soundYes);
+       bg1.add(soundNo);
 
        tablePanel.add(scrollPane);
-       inputPanel.add(addPet);
        inputPanel.add(inputType);
-       inputPanel.add(inputlName);
+       inputPanel.add(inputName);
        inputPanel.add(inputAge);
        inputPanel.add(inputSound);
+       inputPanel.add(soundYes);
+       inputPanel.add(soundNo);
+       buttonPanel.add(addPet);
 
-       listener = new Listener();
-       addPet.addActionListener(listener);
+        addPet.addActionListener(addPetListner);
 
+       frame.getContentPane().add(panel);
+       frame.pack();
+       frame.setVisible(true);
 
    }
 
-   public void addRow(Object[] row) {
+
+    public void addRow(Object[] row) {
        pets.addRow(row);
    }
 
+    private ActionListener addPetListner = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent event) {
 
-   private class Listener implements ActionListener{
+            animalNumber++;
+
+            source = event.getSource();
+            Animal[] animals = new Animal[animalNumber];
+
+            String animalType = (String) inputType.getSelectedItem();
+            String animalName = inputName.getText();
+            int animalAge = Integer.parseInt(inputAge.getText());
+            boolean noiseTrue = Boolean.parseBoolean(soundYes.getText());
+            boolean noiseFalse = Boolean.parseBoolean(soundNo.getText());
+
+            if (inputType.equals("cat")) {
+                animals[animalNumber] = new Cat(animalType, animalName, animalAge, true);
+
+            } else {
+                animals[animalNumber] = new Dog(animalType, animalName, animalAge, true);
+            }
+
+
+        }
+
+    };
+            //animalTyp
+
+            //Animal animal = new Animal(animalType, animalName, animalAge, true);
+//            animal.setType(animalType);
+//            animal.setName(animalName);
+//            animal.setAge(animalAge);
+//            animal.setSound(true);
+
+ }
+
+
+//
+//    public void AddPet(){
+//
+//        AddPet addPetListner = new AddPet();
+//        addPet.addActionListener(addPetListner);
+//
+//    }
+
+
+//    public class AddPet implements ActionListener {
+//
+//
+//        private Object source;
+//
+//
+//        public void actionPerformed(ActionEvent event){
+//
+//            source = event.getSource();
+//            System.out.println(source);
+//            String animalType = inputType.getText();
+//
+//        }
+//
+//    }
 
 
 
-       public void actionPerformed(ActionEvent event){
-
-           source = event.getSource();
-
-       }
-
-   }
 
 
-}
+
