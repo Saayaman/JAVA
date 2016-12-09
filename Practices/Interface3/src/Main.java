efff5f19ac95c172e6bfd33ed8453192cd5c43be
Saayaman <ayakosayama1987@gmail.com>
2016-12-09 13:29:25 -0800

    Erasing comment outs from Animal Table

--- Practices/Interface3/src/Main.java
+++ Practices/Interface3/src/Main.java
@@ -13,36 +13,7 @@ public class Main {
 
         View view = new View();
 
-//        int animalNumber;
-
-         // Animal[] animals = new Animal[]{};
-//
-//                {
-//                        new Dog("husky", "Max", 2, true),
-//                        new Cat("mike", "Tama", 4, true),
-//                };
-
-//        System.out.println(animals[0]);
-//
-//
-
-
-//        int animalLength = view.getAnimalNumber();
-
-
-//        for (int i = 0; i < View.animalNumber; i++) {
-//            view.addRow(
-//                    new Object[]{
-//                            View.animals[i].getType(),
-//                            View.animals[i].getName(),
-//                            View.animals[i].getAge(),
-//                            View.animals[i].getSound(),
-//                    });
-//        }
-
         System.out.println("Hi!");
-
-
 
     }
 }
